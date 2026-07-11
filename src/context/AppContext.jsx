c7061840a3cf7ca7330833d915ccb2b0a6dbc8f6
alisonCosta <alison84cc@gmail.com>
2026-07-11 12:06:25 -0300

    feat: integrate json-server for task management API
    - Added json-server as a dependency and configured it to run on port 3333.
    - Created a db.json file to serve as the database for tasks.
    - Updated AppContext to fetch tasks from the API on load and handle task addition, removal, and editing via API calls.
    - Introduced axios for API requests and created a service for API interactions.

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import {api} from "../services/api"
+
 
 export const AppContext = createContext({}) 
 
@@ -7,32 +9,50 @@ export const AppContextProvider = (props) =>{
 
     const [criador] = useState('Alison')
 
-    const [tarefas, setTarefas ] = useState( [
-        {id: 1 , nome:'Item 1'},
-        {id: 2 , nome:'Item 2'},
-        {id: 3 , nome:'Item 3'}
-    ]
-    ) 
+    const [tarefas, setTarefas ] = useState([])
+    
+    const carregarTarefas = async () =>{
+        try{ const {data} = await api.get('./tarefas')
 
-    const adicionarTarefa = (nomeTarefa) => {
-        setTarefas(estadoAtual => [
-            ...estadoAtual,
-            {
-                id: estadoAtual.length > 0 ? Math.max(...estadoAtual.map(t => t.id)) + 1 : 1,
-                nome: nomeTarefa
-            }
-        ]);
+        setTarefas ( () => data)
+            
+        } catch(erro){
+            console.log("Erro ao carregar as tarefas da API:", erro)
+        }
+    }
+    const adicionarTarefa = async (nomeTarefa) => {
+         try { 
+            const {data} = await api.post("./tarefas", {nome: nomeTarefa})
+            setTarefas(estadoAtual => [
+                ...estadoAtual,
+                data])
+            } catch (erro) {
+                console.error("Erro ao adicionar tarefa na API:", erro);
+            }     
+            // {
+            //     id: estadoAtual.length > 0 ? Math.max(...estadoAtual.map(t => t.id)) + 1 : 1,
+            //     nome: nomeTarefa
+            // }
+        ;
 };
-    const removerTarefa = (idTarefa) => {
+    const removerTarefa = async (idTarefa) => {
+        await api.delete(`./tarefas/${idTarefa}`)
+
         setTarefas(estadoAtual => estadoAtual.filter(tarefa => tarefa.id !== idTarefa));
 };
-    const editarTarefa = (idTarefa, novoNome) =>{
+    const editarTarefa = async (idTarefa, nomeTarefa) =>{
+        const {data: tarefaAtualizada} = await api.put(`./tarefas/${idTarefa}`, {
+            nome: nomeTarefa
+        })
         setTarefas(estadoAtual => estadoAtual.map(tarefa => 
-            tarefa.id  === idTarefa ? {...tarefa, nome: novoNome} : tarefa
+            tarefa.id  === idTarefa ? {...tarefa, nome: tarefaAtualizada.nome} : tarefa
         ) )
 
     }
 
+    useEffect(()=>{
+      carregarTarefas()
+    },[])
 
     return(
         <AppContext.Provider value={{
